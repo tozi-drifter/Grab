70d55cde0aaf5f0084160a0c4ef305d36643c22c
tozi-drifter <154064345+tozi-drifter@users.noreply.github.com>
2024-05-05 08:54:34 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -237,7 +237,7 @@ async def fav(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     """Run bot."""
 
-    application.add_handler(CommandHandler(["guess", "protecc", "collect", "grab", "hunt"], guess, block=False))
+    application.add_handler(CommandHandler(["attain", "protecc", "collect", "grab", "hunt"], guess, block=False))
     application.add_handler(CommandHandler("fav", fav, block=False))
     application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
 
