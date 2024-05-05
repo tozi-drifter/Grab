e657e1f818c81eeacbba8c42adf060f0dbc14f36
tozi-drifter <154064345+tozi-drifter@users.noreply.github.com>
2024-05-05 09:30:57 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    OWNER_ID = "5937246417"
+    sudo_users = "7011799629"
+    GROUP_ID = -1002145420408
+    TOKEN = "6707490163:7152462980:AAFIuHy0xlvPgjO-ZDGWNOGphOItAEtPL6A"
+    mongo_url = "mongodb+srv://ravi:ravi12345@cluster0.hndinhj.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://mallucampaign.in/images/img_1714881106.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
+    SUPPORT_CHAT = "Rulers_Bots_Support"
+    UPDATE_CHAT = "DRIFTERSNETWORK"
+    BOT_USERNAME = "waifuproxbot"
+    CHARA_CHANNEL_ID = "-1002089288943"
+    api_id = 23693234
+    api_hash = "8e084a4919847164a4833587e2eb7e3b"
 
     
 class Production(Config):
